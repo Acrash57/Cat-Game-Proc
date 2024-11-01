274e2ff3225950b96e21c9408fe5a02899fc0d87
Acrash57 <Abdirahman.Ibrahim@laetottenham.ac.uk>
2024-11-01 11:14:31 +0000

    Gave consquences to high intell and energy, used new line functions and added more sleep functions

--- main.py
+++ main.py
@@ -10,29 +10,35 @@ name = input("Enter the name of your cat: ")
 colour = input("What is its colour: ")
 
 while True:
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats 4. Let your cat sleep: 5. Feed your cat: ")
+    option = input("What would you like to do? \n1. Play with your cat \n2. Train your cat \n3. Show stats \n4. Let your cat sleep \n5. Feed your cat: ")
 
-    if option == '1' and cat_attributes["energy"] > 0:
+    if option == '1' and cat_attributes["energy"] >= 0:
         print("Your cat has played!")
+        time.sleep(1) 
         cat_attributes["energy"] -= 1
         cat_attributes["weight"] -= 0.10
+        if cat_attributes["energy"] == 0:
+            print("Your cat is very tired it cannot play again, you might want to let it sleep")
+            time.sleep(1.5)
     elif option == '2':
         print("Your cat has trained!")
-        cat_attributes["intelligence"] += 1
+        time.sleep(1) 
+        cat_attributes["intelligence"] += 2
     elif option == '3':
         print(f"{name}'s Attributes")
         for x in cat_attributes:
             print(f"{x} = {cat_attributes[x]}")
+        time.sleep(1.5) 
     elif option =='4':
         print("Your cat has taken a nap!")
+        time.sleep(1) 
         cat_attributes["energy"] += 2 
     elif option == '5':
         print("Your cat has eaten!")
-        cat_attributes["weight"] += 0.10
+        time.sleep(1) 
+        cat_attributes["weight"] += 0.20
 
-    if cat_attributes['energy'] == 0:
-        print("Your cat is very tired it cannot play again, you might want to let it sleep")
-        time.sleep(1.5)
+
     if cat_attributes["weight"] < 3.5: 
         print(f"{name} is severely underweight and has passed away :(")
         time.sleep(1.5) 
@@ -41,5 +47,14 @@ while True:
     if cat_attributes["weight"] > 6.5: 
         print(f"{name} is severely overweight and has passed away :(")
         time.sleep(1.5)
-        print("Maybe it wasn't a great idea to overfeed your cat")
-        quit() 
+        print(f"Maybe it wasn't a great idea to overfeed {name}")
+        quit()
+    if cat_attributes["intelligence"] >= 70:
+        print(f"{name} has attained high intelligence and found a lost 5 pound note!")
+        time.sleep(1) 
+        print("Congraultions!")
+        time.sleep(1)
+    if cat_attributes["energy"] >= 15:
+        print(f"{name} has too much energy its advised to let it play")
+        time.sleep(1.5) 
+
